cb3ac455fa98c69c337d5d9d1f382243b5da31b3
SaNkeT1025 <73034645+SaNkeT1025@users.noreply.github.com>
2021-10-02 12:30:18 +0530

    Update notification.php

--- php/student/notification.php
+++ php/student/notification.php
@@ -62,7 +62,7 @@ if(!isset($_SESSION["username"]))
             $combine=$_SESSION["username"];
             $arr=explode(" ",$combine);
             $sprn=$arr[0];
-            $connection = mysqli_connect("localhost","root","","college");
+            $connection = mysqli_connect('sql6.freemysqlhosting.net','sql6441142','EmnudfUuyc','sql6441142');
             if($connection->connect_error)
             {
                 die("Error in db connection".$conn->connect_error);
