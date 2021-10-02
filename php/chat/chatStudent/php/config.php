a4461703b91fb8438bfde2b9428fda4fc2b6cfe6
SaNkeT1025 <73034645+SaNkeT1025@users.noreply.github.com>
2021-10-02 12:24:30 +0530

    Update config.php

--- php/chat/chatStudent/php/config.php
+++ php/chat/chatStudent/php/config.php
@@ -4,7 +4,7 @@
   $password = "";
   $dbname = "college";
 
-  $conn = mysqli_connect($hostname, $username, $password, $dbname);
+  $conn = mysqli_connect('sql6.freemysqlhosting.net','sql6441142','EmnudfUuyc','sql6441142');
   if(!$conn){
      echo "Database connection error".mysqli_connect_error();
   }
