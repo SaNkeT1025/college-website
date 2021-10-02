97f22c80922e022817dab8ecfd8816ede839e265
SaNkeT1025 <73034645+SaNkeT1025@users.noreply.github.com>
2021-10-02 11:17:05 +0530

    Update select.php

--- php/admin/select.php
+++ php/admin/select.php
@@ -10,7 +10,7 @@
         if(isset($_POST["faculty_id"]))  
         {  
                 $output = '';  
-                $connect = mysqli_connect("localhost", "root", "", "college");  
+                $connect = mysqli_connect('sql6.freemysqlhosting.net','sql6441142','EmnudfUuyc','sql6441142');  
                 $query = "SELECT * FROM faculty WHERE facultyid = '".$_POST["faculty_id"]."'";  
                 $result = mysqli_query($connect, $query);  
                 $output .= '  
