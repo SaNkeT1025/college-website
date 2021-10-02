f00dd905dbf202ee25e2973c87982d38fb32e6ca
SaNkeT1025 <73034645+SaNkeT1025@users.noreply.github.com>
2021-10-02 12:23:54 +0530

    Update badge.php

--- php/student/badge.php
+++ php/student/badge.php
@@ -3,7 +3,7 @@
     {
         header('Location:../login/login.php');
     }
-    $conn=new mysqli('localhost','root','','college');
+    $conn=new mysqli('sql6.freemysqlhosting.net','sql6441142','EmnudfUuyc','sql6441142');
     if($conn->connect_error)
     {
         die("Error in db connection".$conn->connect_error);
