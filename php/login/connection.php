51492f3857e75bb2e713bccf04247c7a7a716e0b
SaNkeT1025 <73034645+SaNkeT1025@users.noreply.github.com>
2021-10-02 10:57:59 +0530

    Update connection.php

--- php/login/connection.php
+++ php/login/connection.php
@@ -4,7 +4,7 @@
     $password = '';  
     $db_name = "college";  
       
-    $con = mysqli_connect($host, $user, $password, $db_name);  
+    $con = mysqli_connect('sql6.freemysqlhosting.net','sql6441142','EmnudfUuyc','sql6441142');  
     if(mysqli_connect_errno()) {  
         die("Failed to connect with MySQL: ". mysqli_connect_error());  
     }  
